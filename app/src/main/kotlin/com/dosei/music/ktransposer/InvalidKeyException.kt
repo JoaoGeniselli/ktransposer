ece93d5800f7a76baa2f6651b4cb76a985f68335
João Geniselli <joao.g.geniselli@gmail.com>
2022-06-11 10:24:26 -0300

    Doc.

--- app/src/main/kotlin/com/dosei/music/ktransposer/InvalidKeyException.kt
+++ app/src/main/kotlin/com/dosei/music/ktransposer/InvalidKeyException.kt
@@ -1,8 +1,19 @@
 package com.dosei.music.ktransposer
 
+/**
+ * An invalid Key exception during transposition logic.
+ *
+ * Ex.:
+ *
+ * "C#".toNote() = Key.CSharp
+ *
+ * "R".toNote() = InvalidKeyException is thrown
+ *
+ * @property unknownKey the problematic Key.
+ */
 class InvalidKeyException(
-    val note: String
+    val unknownKey: String
 ) : RuntimeException() {
 
-    override val message: String = "$note is unknown in musical scale."
+    override val message: String = "$unknownKey is unknown in musical scale."
 }
